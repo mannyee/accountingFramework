5d2067c37ba17acc1c75df8a2ba7281c65336685
Raj <Raj@Raj-PC>
2015-04-20 17:08:44 -0500

    templating for main page gui

--- src/org/mum/asd/framework/partyPattern/AParty.java
+++ src/org/mum/asd/framework/partyPattern/AParty.java
@@ -1,11 +1,69 @@
 package org.mum.asd.framework.partyPattern;
 
-public abstract class AParty implements IParty{
-	private String name;
-	private String street;
-	private String city;
-	private String state;
-	private String zip;
-	private String email;
+public abstract class AParty implements IParty {
+
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String email;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
 }
---
+++ src/org/mum/asd/framework/partyPattern/Company.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mum.asd.framework.partyPattern;
+
+/**
+ *
+ * @author Raj
+ */
+public class Company extends AParty implements ICompany {
+
+    private Integer employeeNumber;
+
+    public double getBalance() {
+        return 0d;
+    }
+
+    public Integer getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Integer employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+}
