8f962d7056584132beec301862277124cb2164ec
RajeshKtm <developerrajeshyadav@gmail.com>
2015-04-21 15:13:09 -0500

    interest calculation fixed.

--- src/org/mum/asd/client/model/CheckingAccount.java
+++ src/org/mum/asd/client/model/CheckingAccount.java
@@ -6,29 +6,29 @@ import org.mum.asd.framework.transaction.ITransaction;
 
 public class CheckingAccount extends AAccount {
 
-	public CheckingAccount() {
-		super.setType("Ch");
-	}
+    public CheckingAccount() {
+        super.setType("Ch");
+    }
 
-	@Override
-	public void addEntry(ITransaction transaction) {
-		// TODO Auto-generated method stub
+    @Override
+    public void addEntry(ITransaction transaction) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	public double getCurrentBalance() {
-		return super.getBalance();
-	}
+    public double getCurrentBalance() {
+        return super.getBalance();
+    }
 
-	@Override
-	public void setParty(IParty iParty) {
-		super.setParty(iParty);
-		
-	}
+    @Override
+    public void setParty(IParty iParty) {
+        super.setParty(iParty);
+
+    }
 
     @Override
     public Double getInterest() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 5d;
     }
 
 }
--- src/org/mum/asd/client/model/SavingAccount.java
+++ src/org/mum/asd/client/model/SavingAccount.java
@@ -4,31 +4,31 @@ import org.mum.asd.framework.AccountManager.AAccount;
 import org.mum.asd.framework.partyPattern.IParty;
 import org.mum.asd.framework.transaction.ITransaction;
 
-public class SavingAccount extends AAccount{
+public class SavingAccount extends AAccount {
 
-	public SavingAccount() {
+    public SavingAccount() {
         super.setType("S");
     }
-	
-	@Override
-	public void addEntry(ITransaction transaction) {
+
+    @Override
+    public void addEntry(ITransaction transaction) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	public double getCurrentBalance() {
-		return super.getBalance();
-	}
+    }
 
-	@Override
-	public void setParty(IParty iParty) {
-		super.setParty(iParty);
-		
-	}
+    public double getCurrentBalance() {
+        return super.getBalance();
+    }
+
+    @Override
+    public void setParty(IParty iParty) {
+        super.setParty(iParty);
+
+    }
 
     @Override
     public Double getInterest() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 5d;
     }
 
 }
